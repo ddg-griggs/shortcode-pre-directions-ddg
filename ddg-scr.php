f87ecae82c6b76ab83965fd42e14f4904053e12f
DDG <dankovdeveloper@gmail.com>
2025-05-31 10:27:00 +0300

    shortcode-pre-directions-ddg.php

--- ddg-scr.php
+++ ddg-scr.php
@@ -1,27 +1,16 @@
 <?php
-/*
 
+/*
 Plugin Name: Shortcode Pre-Directions by DDG - site go-studio.pro
 Plugin URI: https://github.com/ddg-griggs/shortcode-pre-directions-ddg
-Description: Этот плагин позволяет вам добавлять шорткод на страницу. При выполнении этого шорткода он перенаправляет пользователя на предопределенный URL. Вы также можете установить, сколько секунд ждать перед перенаправлением пользователя. Код для копирования [redirect url='тут ссылка на страницу' sec='3'] вставляется в любое поля с текстом или в шорткод elementor
+Description: Профессиональный виджет перенаправления обратного отсчета для Elementor с полным контролем стиля.
 Author: Dankov Dorel
-Version: 1.0 : 15.03.25
-
+Version: 2.2 : 31.05.25
 */
-add_shortcode('redirect', 'ddg_scr_do_redirect');
-function ddg_scr_do_redirect($atts)
-{
-	ob_start();
-	$myURL = (isset($atts['url']) && !empty($atts['url']))?esc_url($atts['url']):"";
-	$mySEC = (isset($atts['sec']) && !empty($atts['sec']))?esc_attr($atts['sec']):"0";
-	if(!empty($myURL))
-  {
-?>
-		<meta http-equiv="refresh" content="<?php echo $mySEC; ?>; url=<?php echo $myURL; ?>">
-		Пожалуйста, подождите 5 секунд, пока вас перенаправят обратно на сайт… или <a href="<?php echo $myURL; ?>">нажмите здесь,</a> если вы не хотите ждать.
-<?php
-	}
-	return ob_get_clean();
-}
 
-?>
+defined('ABSPATH') || exit;
+
+add_action('elementor/widgets/register', function($widgets_manager) {
+    require_once __DIR__ . '/includes/elementor-widget.php';
+    $widgets_manager->register(new SPDDG_Elementor_Widget());
+});
